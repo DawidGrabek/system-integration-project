38f093da7b19396ce02e857f65ebff5d48c3a35b
Dawid Grabek <grabekd37@gmail.com>
2023-06-11 22:59:50 +0200

    Add export/import json/xml

--- client/src/components/organisms/DataExchange/DataExchange.js
+++ client/src/components/organisms/DataExchange/DataExchange.js
@@ -2,6 +2,7 @@ import Button from 'components/atoms/Button/Button'
 import React, { useState } from 'react'
 import { DataExchangeItem, Wrapper } from './DataExchange.styles'
 import AxiosApi from 'axios.config'
+
 const exportToFileJSON = (data, fileName) => {
   const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
     JSON.stringify(data, fileName)
@@ -9,6 +10,15 @@ const exportToFileJSON = (data, fileName) => {
   const link = document.createElement('a')
   link.href = jsonString
   link.download = `${fileName}.json`
+
+  link.click()
+}
+
+const exportToFileXML = (data, fileName) => {
+  const xmlString = `data:text/xml;chatset=utf-8,${encodeURIComponent(data)}`
+  const link = document.createElement('a')
+  link.href = xmlString
+  link.download = `${fileName}.xml`
 
   link.click()
 }
@@ -27,6 +37,20 @@ const readJsonFile = (file) =>
     fileReader.readAsText(file)
   })
 
+const readXMLFile = (file) =>
+  new Promise((resolve, reject) => {
+    const fileReader = new FileReader()
+
+    fileReader.onload = (event) => {
+      if (event.target) {
+        resolve(event.target.result)
+      }
+    }
+
+    fileReader.onerror = (error) => reject(error)
+    fileReader.readAsText(file)
+  })
+
 const DataExchange = ({
   inflation,
   expenseExpenditure,
@@ -34,93 +58,99 @@ const DataExchange = ({
   setInflation,
 }) => {
   const [selectedYear, setSelectedYear] = useState('1995')
+  const [selectedYearJSON, setSelectedYearJSON] = useState('1995')
   const handleYearChange = (event) => {
     setSelectedYear(event.target.value)
   }
 
-  const handleExportXML = async () => {
-    let stringRequest = 
-    `<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tns="http://system_integration.pl/soap_service">
+  // ZROBIONE
+  const handleImportXML = async (event) => {
+    if (event.target.files) {
+      const parsedData = await readXMLFile(event.target.files[0])
+
+      const response = await AxiosApi.post('/api/v1/ws', parsedData, {
+        headers: {
+          'content-type': 'text/xml',
+        },
+      })
+    }
+  }
+
+  // ZROBIONE
+  const handleExportJSON = async () => {
+    const { data } = await AxiosApi.get(`/api/v1/inflation/${selectedYearJSON}`)
+    exportToFileJSON(data, `inflation${selectedYearJSON}`)
+
+    // exportToFileJSON(inflation, 'inflation')
+    // exportToFileJSON(expenseExpenditure, 'expenseExpenditure')
+    // exportToFileJSON(expenseProduct, 'expenseProduct')
+  }
+
+  // ZROBIONE
+  const handleImportJSON = async (event) => {
+    if (event.target.files) {
+      const parsedData = await readJsonFile(event.target.files[0])
+      const mergedData = [...inflation, parsedData]
+
+      await AxiosApi.post('/api/v1/inflation', parsedData)
+      setInflation(mergedData)
+    }
+  }
+
+  // ZROBIONE
+  const handleExportXMLYear = async () => {
+    const stringRequest = `<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tns="http://system_integration.pl/soap_service">
     <soap:Header/>
     <soap:Body>
       <tns:getExpense>
-        <year>2005</year>
+        <year>${selectedYear}</year>
         <unitTitle>Total General Government Expenditure</unitTitle>
       </tns:getExpense>
     </soap:Body>
   </soap:Envelope>
-  `;
+  `
     const response = await AxiosApi.post('/api/v1/ws', stringRequest, {
       headers: {
-        'content-type': 'text/xml'
-      }
+        'content-type': 'text/xml',
+      },
     })
-    console.log(response.data)
-  }
 
-  const handleImportXML = async () => {
-    // zastąpić plikiem
-    let stringRequest = 
-    `<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tns="http://system_integration.pl/soap_service">
-    <soap:Header/>
-    <soap:Body>
-      <tns:addExpense>
-        <year>2025</year>
-        <value>85</value>
-        <unit>
-          <unit>Percent</unit>
-          <title>Total General Government Expenditure</title>
-        </unit>
-        <country>
-          <name>Poland</name>
-          <code>PL</code>
-        </country>
-      </tns:addExpense>
-    </soap:Body>
-  </soap:Envelope>
-  `;
-    const response = await AxiosApi.post('/api/v1/ws', stringRequest, {
-      headers: {
-        'content-type': 'text/xml'
-      }
-    })
-    console.log(response.data)
-  }
-
-  const handleExportJSON = () => {
-    exportToFileJSON(inflation, 'inflation')
-    exportToFileJSON(expenseExpenditure, 'expenseExpenditure')
-    exportToFileJSON(expenseProduct, 'expenseProduct')
-  }
-
-  const handleImportJSON = async (event) => {
-    console.log('handleImportJSON')
-    if (event.target.files) {
-      const parsedData = await readJsonFile(event.target.files[0])
-      const mergedData = [...inflation, ...parsedData]
-      // console.log(mergedData)
-      setInflation(mergedData)
-      // console.log('inflation', inflation)
-    }
-  }
-
-  const handleExportXMLYear = () => {
-    console.log('handleExportXMLYear', selectedYear)
+    exportToFileXML(response.data, `GrossExpenditure${selectedYear}`)
   }
 
   return (
     <Wrapper>
       <DataExchangeItem>
-        <span>Export XML:</span>{' '}
-        <Button onClick={handleExportXML}>Export</Button>
-      </DataExchangeItem>
-      <DataExchangeItem>
-        <span>Import XML:</span>{' '}
-        <Button onClick={handleImportXML}>Import</Button>
-      </DataExchangeItem>
-      <DataExchangeItem>
         <span>Export JSON:</span>{' '}
+        <select
+          name="jsonYear"
+          id="jsonYear"
+          value={selectedYearJSON}
+          onChange={(e) => setSelectedYearJSON(e.target.value)}
+        >
+          {inflation.map(({ year }, i) => (
+            <option key={i} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
         <Button onClick={handleExportJSON}>Export</Button>
+      </DataExchangeItem>
+      <DataExchangeItem>
+        <span>Export XML:</span>
+        <select
+          name="xmlYear"
+          id="xmlYear"
+          value={selectedYear}
+          onChange={handleYearChange}
+        >
+          {expenseExpenditure.map(({ year }, i) => (
+            <option key={i} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+        <Button onClick={handleExportXMLYear}>Export</Button>
       </DataExchangeItem>
       <DataExchangeItem>
         <span>Import JSON:</span>{' '}
@@ -133,21 +163,16 @@ const DataExchange = ({
           onChange={handleImportJSON}
         />
       </DataExchangeItem>
-
       <DataExchangeItem>
-        <span>Import XML:</span>
-        <select
-          name="xmlYear"
-          id="xmlYear"
-          value={selectedYear}
-          onChange={handleYearChange}
-        >
-          <option value="1995">1995</option>
-          <option value="1996">1996</option>
-          <option value="1997">1997</option>
-          <option value="1998">1998</option>
-        </select>
-        <Button onClick={handleExportXMLYear}>Import</Button>
+        <span>Import XML:</span>{' '}
+        {/* <Button onClick={handleImportXML}>Import</Button> */}
+        <input
+          type="file"
+          name="ImportXML"
+          id="ImportXML"
+          accept=".xml"
+          onChange={handleImportXML}
+        />
       </DataExchangeItem>
     </Wrapper>
   )
